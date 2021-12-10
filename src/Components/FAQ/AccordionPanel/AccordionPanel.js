4589cfd00ea10c57ecb14fe98b1523d3458dd4b7
Muhammad Bin Aamir <Muhammad.amir@brilliantgamez.com>
2021-12-10 16:36:06 +0500

    Added dark theme

--- src/Components/FAQ/AccordionPanel/AccordionPanel.js
+++ src/Components/FAQ/AccordionPanel/AccordionPanel.js
@@ -1,39 +1,39 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import Accordion from "@mui/material/Accordion";
+
 import { AccordionSummary, AccordionDetails } from "@mui/material";
+
 import AddIcon from "@mui/icons-material/Add";
-import RemoveIcon from '@mui/icons-material/Remove';
+import RemoveIcon from "@mui/icons-material/Remove";
 import { Typography } from "@mui/material";
 
+const AccordionPanel = (props) => {
 
-const AccordionPanel = props => {
-    const [expandedState, setExpandedState] = useState(false);
-   
-     const handleAccordionToggle = () => {
-         setExpandedState( prev =>{
-             return !expandedState
-         })
-     };
+  
 
-    
-    return(
-      <Accordion onChange={()=> handleAccordionToggle()} >
-      <AccordionSummary expandIcon={ expandedState? <RemoveIcon/>: <AddIcon />} >
-      <Typography>
-          Who are the Monsters?
+  const [expandedState, setExpandedState] = useState(false);
+
+  const handleAccordionToggle = () => {
+    setExpandedState((prev) => {
+      return !expandedState;
+    });
+  };
+
+  return (
+      
+    <Accordion disableGutters onChange={() => handleAccordionToggle()}>
+      <AccordionSummary
+        expandIcon={expandedState ? <RemoveIcon /> : <AddIcon />}
+      >
+        <Typography variant="h6" component="p">
+          {props.question}
         </Typography>
       </AccordionSummary>
       <AccordionDetails>
-        <Typography>
-          We are a dedicated team of developers with creative
-          thinking and pushing boundaries.
-        </Typography>
+        <Typography>{props.answer}</Typography>
       </AccordionDetails>
-
     </Accordion>
-  
-    )
-
-}
+  );
+};
 
 export default AccordionPanel;
--- src/Components/FAQ/FAQ.js
+++ src/Components/FAQ/FAQ.js
@@ -1,13 +1,43 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 import AccordionPanel from "./AccordionPanel/AccordionPanel";
+import Container from "@mui/material/Container";
+import { useTheme, ThemeProvider, createTheme } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const FAQ = (props) => {
+  const darkTheme = useMemo(() =>
+    createTheme({
+      palette: {
+        mode: "dark",
+        background: {
+          default: "#222222",
+        },
+      },
+    })
+  );
+
+  const [questionsState, setQuestionsState] = useState([
+    "Who are the monsters?",
+    "What are your future plans?",
+    "What will be the price?",
+  ]);
+
+  const [answersState, setAnswersState] = useState([
+    "The monsters are a wonderful new group coming to your town in the near future",
+    "We will invest in Artists, Game Developers and Creative Content Producers to expand our universe",
+    "The Price is yet to be decided by the Monsters",
+  ]);
 
   return (
-    <div>
-        <AccordionPanel/>
-     </div>
+    <ThemeProvider theme={darkTheme}>
+        <CssBaseline/>
+      <Container>
+        {questionsState.map((item, i) => {
+          return <AccordionPanel question={item} answer={answersState[i]} />;
+        })}
+      </Container>
+    </ThemeProvider>
   );
 };
 
